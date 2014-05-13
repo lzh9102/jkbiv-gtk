178929bdaf80d8ee1469642023b0703b23a25dab
Che-Huai Lin <lzh9102@gmail.com>
2014-05-13 19:45:36 -0400

    Insulate Application() from Gtk

--- jkbiv/app.py
+++ jkbiv/app.py
@@ -1,8 +1,6 @@
 from baseapp import BaseApplication
-import gtk
 
 class Application(BaseApplication):
 
-    def onKeyPress(self, widget, event):
-        keyname = gtk.gdk.keyval_name(event.keyval)
-        print keyname
+    def onKeyPress(self, keystr):
+        print "key: %s" % keystr
--- jkbiv/baseapp.py
+++ jkbiv/baseapp.py
@@ -21,12 +21,29 @@ class BaseApplication(object):
         self.height = height
 
         # events
-        window.connect("key_press_event", self.onKeyPress)
+        window.connect("key_press_event", self.__handleKeyPress)
 
         window.show_all()
 
+    def __isModifier(self, keystr):
+        # TODO: improve efficiency by using numeric value
+        return keystr.startswith("control_") or keystr.startswith("shift_") \
+                or keystr.startswith("meta_") or keystr.startswith("alt_")
+
+    def __handleKeyPress(self, widget, event):
+        keystr = gtk.gdk.keyval_name(event.keyval).lower() # key name
+        if self.__isModifier(keystr): # ignore modifier keys
+            return
+        if event.state & gtk.gdk.SHIFT_MASK: # with shift key
+            keystr = "S-" + keystr
+        if event.state & gtk.gdk.CONTROL_MASK: # with control key
+            keystr = "C-" + keystr
+        if event.state & gtk.gdk.MOD1_MASK: # with alt (meta) key
+            keystr = "A-" + keystr
+        self.onKeyPress(keystr)
+
     # event callbacks
 
-    def onKeyPress(self, widget, event):
+    def onKeyPress(self, keystr):
         """ this function will be called when a key is pressed """
         pass
