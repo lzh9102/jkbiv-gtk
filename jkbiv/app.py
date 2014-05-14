5e347100c0eb18b034fa0030cee8a3916e18256d
Che-Huai Lin <lzh9102@gmail.com>
2014-05-13 20:55:30 -0400

    Integrate DirectoryWalker into application

--- jkbiv/app.py
+++ jkbiv/app.py
@@ -1,5 +1,8 @@
 from baseapp import BaseApplication
 from shortcut import ShortcutMapper, Keystroke
+from resource import DirectoryWalker
+import sys
+import os
 
 class Application(BaseApplication):
 
@@ -7,6 +10,12 @@ class Application(BaseApplication):
         super(Application, self).__init__(width, height)
         self.keymap = ShortcutMapper()
         self.setupKeymaps()
+        if len(sys.argv) >= 2:
+            directory = sys.argv[1]
+        else:
+            directory = os.getcwd()
+        self.dirwalker = DirectoryWalker(directory)
+        self.redrawAll()
 
     def setupKeymaps(self):
         keymap = self.keymap
@@ -14,6 +23,23 @@ class Application(BaseApplication):
         KSL = lambda keys: [Keystroke(key) for key in keys]
         # setup keymaps
         keymap.bind(KSL("q"), self.quit)
+        keymap.bind(KSL("l"), self.fnNext)
+        keymap.bind(KSL("h"), self.fnPrev)
 
     def onKeyPress(self, keystr):
         self.keymap.pressKey(Keystroke(keystr))
+
+    def redrawAll(self):
+        res = self.dirwalker.currentResource()
+        if res:
+            print "resource: %s" % res.getUrl()
+
+    # user-reachable functions
+
+    def fnNext(self):
+        if self.dirwalker.next():
+            self.redrawAll()
+
+    def fnPrev(self):
+        if self.dirwalker.prev():
+            self.redrawAll()
