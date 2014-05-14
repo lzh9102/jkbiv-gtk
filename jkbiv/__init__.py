7019640b37af836689cb753c145435101d0ecfea
Che-Huai Lin <lzh9102@gmail.com>
2014-05-14 10:32:56 -0400

    Add profiling code

--- jkbiv/__init__.py
+++ jkbiv/__init__.py
@@ -1,6 +1,16 @@
 import gtk
+import cProfile
 from app import Application
 
-def main():
+def run():
     app = Application(width=800, height=600)
     gtk.main()
+
+def main():
+    profile = False
+    if profile:
+        profiler = cProfile.Profile()
+        profiler.runcall(run)
+        profiler.print_stats(sort=1)
+    else:
+        run()
